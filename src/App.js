53f3be01f0724d05dbd797a234de29e8858575c5
Mauricio Cantu <ciciocantuu@gmail.com>
2020-05-21 14:20:44 -0300

    finished challenge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      url: "https://github.com/mauricio-cantu",
+      title: `Desafio ReactJS ${Date.now()}`,
+      techs: ["Javascript", "Elixir"],
+    };
+
+    try {
+      const response = await api.post("repositories", data);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+    } catch (error) {
+      alert("Erro ao cadastrar repositório.");
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch (error) {
+      alert("Erro ao deletar repositório.");
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
